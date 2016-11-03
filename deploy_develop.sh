8b3676d8f407b3d0b2540a3dab44e40273b7987d
muradisayevzipmend <murad.isayev@zipmend.com>
2016-11-03 18:55:19 +0100

    sdfdsf

--- deploy_develop.sh
+++ deploy_develop.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "success"
+lastCommit=$(git show --summary)
+	arr=($lastCommit)
+	lastCommit=${arr[4]}
+echo $lastCommit
+
+lastBuild=$(<last_build)
+
+changedFiles=$(git diff --no-commit-id --name-only -r $lastBuild $lastCommit)
+
+echo $changedFiles
