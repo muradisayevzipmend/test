e0cc27f873e56483ab42ca23b5329dc1a94b4db1
muradisayevzipmend <murad.isayev@zipmend.com>
2016-11-04 10:52:57 +0100

    asdasd

--- deploy_develop.sh
+++ deploy_develop.sh
@@ -10,6 +10,6 @@ changedFiles=$(git diff --no-commit-id --name-only -r $lastBuild $commitId)
 
 echo $changedFiles
 
-echo "Storing last commit id"
+echo "Storing lastt commit id"
 export LAST_BUILD=$commitId
 echo "Last build id is now: "$LAST_BUILD
